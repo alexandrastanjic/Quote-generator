14975ac52dea208dfd8bb14e320a51fbb5780cc6
Alexandra S <alexandrastanjic@gmail.com>
2020-12-27 15:07:55 +0200

    Finish UI and logic

--- resources/script.js
+++ resources/script.js
@@ -3,8 +3,22 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('newQuote');
+const loader = document.getElementById('loader');
+
+ function showLoadingSpinner() {
+   loader.hidden = false;
+   quoteContainer.hidden = true;
+ }
+
+ function removeLoadingSpinner() {
+   if (!loader.hidden) {
+     quoteContainer. hidden = false;
+     loader.hidden = true
+   }
+ }
 
 async function getQuote() {
+  showLoadingSpinner();
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
   const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
@@ -23,9 +37,24 @@ if (data.quoteText.length > 120) {
   quoteText.classList.remove('long-quote');
 }
     quoteText.innerText = data.quoteText;
+
+    removeLoadingSpinner();
   } catch (error) {
     getQuote();
   } 
 }
+
+// Tweet Quote
+
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text= ${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+// Event Listener
+
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 getQuote();
